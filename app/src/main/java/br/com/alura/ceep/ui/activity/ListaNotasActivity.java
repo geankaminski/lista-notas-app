dd62f9747c8b8f9dd0ab81be3078dc8cb0f4aa11
Gean Kaminski <geankaminski93@hotmail.com>
2021-03-19 08:08:49 -0300

    onResume update

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -36,6 +36,14 @@ public class ListaNotasActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onResume() {
+        NotaDAO dao = new NotaDAO();
+        List<Nota> todasNotas = dao.todos();
+        configuraRecyclerView(todasNotas);
+        super.onResume();
+    }
+
     private List<Nota> notasDeExemplo() {
         NotaDAO dao = new NotaDAO();
         dao.insere(new Nota("Título ", "Descrição "),
